2c4b89c57c95dc8ccd3870c52a5aefd2c84baa86
sanachi1027 <sana.yosida.0525@icloud.com>
2024-05-15 20:51:58 +0900

    Create 222.css

---
+++ 222.css
@@ -0,0 +1,42 @@
+@charset "utf-8";
+body{background-color: lightblue;}
+body{text-align: center;}
+h1 {color: hotpink;}
+h3 {ul,ol{color:#1e366a; border-top:solid #1e366a  1px;
+border-bottom:solid #1e366a 1px;
+padding: 0.5em 0 0.5em 1.5em;}
+ul li,ol li{line-height: 1.5;
+padding: 0.5em 0;}}
+
+img {width: 350px;height: 220px;}
+img {
+  display: inline-block;
+  padding: 1px;
+  background: #000000;
+  box-shadow: 5px 5px 3px #bbbbbb;
+}
+ img {
+  padding: 10px 10px 30px;
+  background: #fff;
+}
+
+h1 {
+  position: relative;
+  color: #f864e4;
+  display: inline-block;
+  margin: 47px 0;
+  text-shadow: 0 0 2px white;
+}
+h1:before {
+  content: "";
+  position: absolute;
+  background: #f8e160;
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  top: 50%;
+  left: 50%;
+  -webkit-transform: translate(-50%,-50%);
+  transform: translate(-50%,-50%);
+  z-index: -1;
+}
